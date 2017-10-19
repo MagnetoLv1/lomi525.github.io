efed582ffd9e55924689cf0d915460be52cc5bde
김광철 <anyman@afreecatv.com>
2017-10-19 10:42:05 +0900

    검색 ui 수정

--- js/search.js
+++ js/search.js
@@ -51,8 +51,8 @@ MIT Licensed
   }
 
   window.toggleSearch = function toggleSearch() {
-    searchEl.classList.toggle('is-active');
-    if (searchEl.classList.contains('is-active')) {
+    searchEl.classList.toggle('devsite-popout-closed');
+    if (searchEl.classList.contains('devsite-popout-closed')) {
       // while opening
       searchInputEl.value = '';
     } else {
@@ -68,9 +68,10 @@ MIT Licensed
     var currentResultHash, d;
 
     currentInputValue = searchInputEl.value;
-    if (!currentInputValue || currentInputValue.length < 3) {
+    if (!currentInputValue || currentInputValue.length < 2) {
       lastSearchResultHash = '';
-      searchResultsEl.classList.add('is-hidden');
+      searchEl.classList.add('devsite-popout-closed');
+      searchResultsEl.innerHTML = '';
       return;
     }
     searchResultsEl.style.offsetWidth;
@@ -80,19 +81,68 @@ MIT Licensed
         return true;
       }
     });
-    if (!matchingPosts.length) {
-      // searchResultsEl.classList.add('is-hidden');
-      searchEl.classList.toggle('is-active');
+
+    if (matchingPosts.length) {
+      searchEl.classList.remove('devsite-popout-closed');
     }
     currentResultHash = matchingPosts.reduce(function(hash, post) { return post.title + hash; }, '');
     if (matchingPosts.length && currentResultHash !== lastSearchResultHash) {
       searchResultsEl.classList.remove('is-hidden');
       searchResultsEl.innerHTML = matchingPosts.map(function (post) {
         d = new Date(post.pubDate);
-        return '<li><a href="' + post.link + '">' + post.title + ' - <span class="search__result-date">' + d.toUTCString().replace(/.*(\d{2})\s+(\w{3})\s+(\d{4}).*/,'$2 $1, $3') + '</span></a></li>';
+        //return '<li><a href="' + post.link + '">' + post.title + ' - <span class="search__result-date">' + d.toUTCString().replace(/.*(\d{2})\s+(\w{3})\s+(\d{4}).*/,'$2 $1, $3') + '</span></a></li>';
+        return '<div class="goog-menuitem devsite-suggest-item" role="menuitem" id=":b">'+
+        '<div class="goog-menuitem-content">'+
+          '<a class="devsite-suggestion" href="' + post.link + '">'+
+            '<span class="devsite-suggestion-fragment">' + post.title + '</span><span class="devsite-suggestion-fragment">' + post.category + '</span>'+
+          '</a></div></div>'
+
       }).join('');
     }
     lastSearchResultHash = currentResultHash;
+  }
+
+  var focusIndex = -1;
+  function keyFocusDown(){
+      var items = document.querySelectorAll('.devsite-suggest-item');
+      if(!items.length){
+        return;
+      }
+
+      if(focusIndex>=0 && items[focusIndex])
+      {
+        items[focusIndex].classList.remove('goog-menuitem-highlight');
+      }
+      if(items.length-1 <= focusIndex){
+        focusIndex = -1;
+      }
+      console.log(items.length, focusIndex)
+      items[++focusIndex].classList.add('goog-menuitem-highlight');
+  }
+  function keyFocusUp(){
+      var items = document.querySelectorAll('.devsite-suggest-item');
+      if(!items.length){
+        return;
+      }
+
+      if(focusIndex>=0 && items[focusIndex])
+      {
+        items[focusIndex].classList.remove('goog-menuitem-highlight');
+      }
+      if( focusIndex <= 0){
+        focusIndex = items.length;
+      }
+      console.log(items.length, focusIndex)
+      items[--focusIndex].classList.add('goog-menuitem-highlight');
+  }
+  function keyFocusEnter(){  
+
+    var items = document.querySelectorAll('.devsite-suggest-item');
+    if(items[focusIndex])
+    {
+      var href = items[focusIndex].querySelector('.devsite-suggestion').getAttribute("href");
+      location.href = href;
+    }
   }
 
   function init(options) {
@@ -126,8 +176,23 @@ MIT Licensed
       }
     });
 
-    searchInputEl.addEventListener('input', function onInputChange() {
+    searchInputEl.addEventListener('input', function onInputChange(e) {
       handleInput();
+    });
+    searchInputEl.addEventListener('keydown', function onKeyPress(e) {
+      console.log(e.which)
+      switch(e.which)
+      {
+        case 40:
+          keyFocusDown();
+          break;        
+        case 38:
+          keyFocusUp();
+          break;
+          case 13:
+          keyFocusEnter();
+          break;
+      }
     });
   }
 
